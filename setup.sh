e82a2185f010226073d5ae5c70ba41ef56796414
Nigel Banks <nigel.g.banks@gmail.com>
2021-01-21 15:04:06 +0000

    Certs are messed up

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ git clone --recurse-submodules -j4 https://github.com/asulibraries/islandora-rep
 pushd islandora-repo
 git apply ../0001-php-downgrade-patch.txt
 popd
+mkdir certs > /dev/null
+pushd certs
 wget "http://traefik.me/fullchain.pem"
 wget "http://traefik.me/privkey.pem"
+popd
 export DOCKER_BUILDKIT=1 
 export COMPOSE_DOCKER_CLI_BUILD=1
 docker-compose build
